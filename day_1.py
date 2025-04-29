8d5523b9dab3f70519e13f17a19050bd09e3a8e8
Saad <154870199+saad-726@users.noreply.github.com>
2025-04-29 19:27:42 +0500

    All task of day 1 completed

---
+++ day_1.py
@@ -0,0 +1,8 @@
+age=17
+height=5.4
+name="Saad"
+
+print(f"Name is : {name} ,Age is : {age} and Height is {height}")
+#it prints the name, age and height of a person using variable of different data types (int/float/string)
+age=21#updating value in a variable
+print(f"Name is : {name} ,Age is : {age} and Height is {height}")
