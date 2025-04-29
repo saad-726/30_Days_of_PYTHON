c2de1f52e1a9237d8fd228188fc8ed2d98fc21a4
Saad <154870199+saad-726@users.noreply.github.com>
2025-04-29 19:38:55 +0500

    Done with day 2 (Input/Output)

---
+++ day_2.py
@@ -0,0 +1,11 @@
+name=input("Enter your name :")
+YOB=int(input("Enter your birth Year :"))
+print(f"Hey {name}, Welcome!")
+age=2025-YOB
+print(f"You will be {age+1} years old next year!")
+#input two numbers and print their sum
+
+x=int(input("Enter a number :"))
+y=int(input("Enter another number :"))
+sum=x+y
+print("The sum of numbers is :",sum)
