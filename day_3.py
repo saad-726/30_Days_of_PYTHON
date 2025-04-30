7603f9af377be617df0d9a17edfcf058c72d546e
Saad <154870199+saad-726@users.noreply.github.com>
2025-04-30 12:21:54 +0500

    Done with Day 3 question (Basic Operators)

---
+++ day_3.py
@@ -0,0 +1,18 @@
+num1=int(input("Enter a number :"))
+num2=int(input("Enter another number :"))
+
+print ("The addition of the two numbers is :",num1+num2)
+print("The multiplication is :",num1*num2)
+
+#finding remainder
+rem=45%7
+print("The remiander is :",rem)
+
+#updating values using += and *=
+
+a=7
+b=12
+print("Before updation :",a,b)
+a+=1 #a=a+1 ==8
+b*=2 #b=b*2 ==24
+print("After updation :",a,b)
